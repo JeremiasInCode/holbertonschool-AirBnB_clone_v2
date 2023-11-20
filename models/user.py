a83b420a37883bc468f75d5731de15077384cfbf
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-20 19:12:45 -0300

    Add permissions and fix error pycodestyle

--- models/user.py
+++ models/user.py
@@ -6,13 +6,15 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """ User class with various attributes """
+    """ User class """
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
     places = relationship("Place", backref="users",
                           cascade="all, delete-orphan")
+
     reviews = relationship("Review", backref="users",
                            cascade="all, delete-orphan")
