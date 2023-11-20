6ce22652dbc4dbf326dab2ba760672974dc662c5
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-20 19:07:48 -0300

    Add permissions and fix error pycodestyle

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel, Base):
     """ User class with various attributes """
     __tablename__ = 'users'
@@ -11,5 +12,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    
