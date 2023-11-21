8774539083fb985b1dca18540703a89c94c604bb
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-21 16:32:05 -0300

    Fix environment

--- models/amenity.py
+++ models/amenity.py
@@ -6,9 +6,12 @@ from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class Amenity(BaseModel, Base):
-    """ Amenity class """
+    """ Amenitys of the Place"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenity = relationship("Place", secondary=place_amenity,
-                                back_populates="amenities")
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
