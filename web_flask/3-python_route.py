d6aa74b3402b254225e386a22a22232064ef6a50
JereDev19 <jeremiaserba7894@gmail.com>
2023-12-16 12:16:09 -0300

    fixes

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,21 +3,22 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def hello_world():
     """ Displays 'Hello HBNB!' """
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
     """ Displays 'HBNB!' """
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def display_text_c(text):
     """ Displays 'C' <text> """
     text_replaced = text.replace("_", " ")
