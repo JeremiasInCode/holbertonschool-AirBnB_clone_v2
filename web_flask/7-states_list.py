c9baec706c4f895c216ff91df5c663240129a7a2
JereDev19 <jeremiaserba7894@gmail.com>
2023-12-17 20:34:19 -0300

    The lasted try

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
-
-""" script that starts a Flask web application
-    defines routes to handle specific urls
-    fetching data from the storage engine
-"""
-
-from flask import Flask
-from flask import render_template
-import models
+"""List of states"""
+from flask import Flask, render_template
+from models import storage
 from models.state import State
-
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route("/states_list")
-def states_list():
-    """Returns a state list of elements"""
-    d_states = models.storage.all(State)
-    return render_template("7-states_list.html", stateslist=d_states.values())
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """close of storage of all models"""
-    models.storage.close()
+def app_teardown_appcontext(self):
+    "Close the session after each request"
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    return render_template("7-states_list.html",
+                           states=storage.all(State).values())
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
