01b9abedc85460c96d8a3234ec451924a13f0551
JereDev19 <jeremiaserba7894@gmail.com>
2023-12-17 20:55:59 -0300

    Task 6 - fixess

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -5,25 +5,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-
 @app.route("/")
 def hello_world():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
-
 
 @app.route("/hbnb")
 def hbnb():
     """Displays 'HBNB!'"""
     return "HBNB"
 
-
 @app.route("/c/<text>")
 def display_text_c(text):
     """Returns a text"""
     text = text.replace("_", " ")
     return f"C {text}"
-
 
 @app.route("/python")
 @app.route("/python/<text>")
@@ -32,18 +28,16 @@ def display_text_python(text="is cool"):
     text = text.replace("_", " ")
     return f"Python {text}"
 
-
 @app.route("/number/<int:n>")
 def display_number(n):
     """Returns an HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
-
 @app.route("/number_odd_or_even/<int:n>")
-def evenSo(n):
+def even_or_odd(n):
     """Returns an html page depending on the parity of n"""
-    return render_template("6-number_odd_or_even.html", n=n)
-
+    parity = "even" if n % 2 == 0 else "odd"
+    return render_template("6-number_odd_or_even.html", n=n, parity=parity)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
