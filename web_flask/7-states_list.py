7cb1a3ef1105c9ffdda001018a204a010c5ea109
JereDev19 <jeremiaserba7894@gmail.com>
2023-12-17 11:43:02 -0300

    Task 7 fixess

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
-
-This module contains a Flask web application that displays a list of states.
-"""
+"""List of states"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def statesList():
-    """Displays an HTML page"""
-    return render_template("7-states_list.html",
-                           states=storage.all(State).values())
-
-
 @app.teardown_appcontext
-def tearDown(arg=None):
-    """Ends the current session."""
+def close_session(arg=None):
+    "Close the session after each request"
     storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """gets all of the State objects from the storage engine"""
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
 
 
 if __name__ == "__main__":
