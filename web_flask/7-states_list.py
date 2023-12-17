d3ee59644e788b59901d6fb634116cc8d9bfdfb1
JereDev19 <jeremiaserba7894@gmail.com>
2023-12-17 19:55:06 -0300

    Fixes in task y7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
+""" script that starts a Flask web application
+    defines routes to handle specific urls
+    fetching data from the storage engine
 """
-Starts a Flask web application.
-"""
-
-from flask import Flask, render_template
-from models import storage
+from flask import Flask
+from flask import render_template
+import models
 from models.state import State
+
 
 app = Flask(__name__)
 
-@app.teardown_appcontext
-def teardown(e):
-    storage.close()
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    d_states = models.storage.all(State)
+    return render_template("7-states_list.html", stateslist=d_states.values())
+
+
+@app.teardown_appcontext
+def teardown(self):
+    models.storage.close()
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
