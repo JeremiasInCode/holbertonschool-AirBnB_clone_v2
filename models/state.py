a5a02bb2112abf379d2310f30e6ce45c058a1332
JereDev19 <jeremiaserba7894@gmail.com>
2023-12-17 11:33:09 -0300

    Fixes

--- models/state.py
+++ models/state.py
@@ -2,22 +2,24 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
 from os import getenv
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
+    name = Column(String(60), nullable=False)
 
     if getenv("HBNB_MYSQL_DB") == 'db':
-        cities = relationship("City", backref="state",
-                          cascade='all, delete-orphan')
+        cities = relationship("City", cascade="all, delete", backref="state")
     else:
+        @property
         def cities(self):
+            """Getter method"""
             from models import storage
+
             listOfCities = []
             for city in storage.all(City).values():
                 if city.state_id == self.id:
