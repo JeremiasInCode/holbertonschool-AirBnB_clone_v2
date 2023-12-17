2e5cec575e590adb2c2af668f7a7beb33e7a10e5
JereDev19 <jeremiaserba7894@gmail.com>
2023-12-17 11:51:32 -0300

    Fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,17 +7,16 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_session(arg=None):
+def app_teardown_appcontext(self):
     "Close the session after each request"
     storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """gets all of the State objects from the storage engine"""
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
+    return render_template("7-states_list.html",
+                           states=storage.all(State).values())
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
